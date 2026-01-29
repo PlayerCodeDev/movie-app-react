9d7657d02aac640e2da42d6d596b4e459ee0176b
PlayerCodeDev <o.ramirez.carlos@gmail.com>
2026-01-29 16:12:44 -0300

    feat: agregar configuracion del proyecto y header con buscador

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+import { useState } from 'react';
+import Search from './components/Search.jsx';
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <main>
+      <div className='pattern' />
+
+      <div className='wrapper'>
+
+        <header>
+          <img src="./hero-img.png" alt="Hero banner" />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1>{searchTerm}</h1>
+      </div>
+    </main>
+  )
+};
+
+export default App;
